de91ec9953b48c3c931883ed0ab04d4362e34901
Jason <atxjdc@gmail.com>
2019-03-26 18:04:28 +0000

    start script

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
-var redis = require("redis");
+var redis = require("redis").createClient();
 
 var app = express();
 
