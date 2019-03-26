27a62ee7a766dbccc1f4a0354a5cf00a1cd77b47
Jason <atxjdc@gmail.com>
2019-03-26 18:27:11 +0000

    t4

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ var redis = require("redis");
 var app = express();
 
 if (process.env.REDISTOGO_URL) {
-
-} else {
     var rtg   = require("url").parse(process.env.REDISTOGO_URL);
     var redis = require("redis").createClient(rtg.port, rtg.hostname);
 
     redis.auth(rtg.auth.split(":")[1]);
+} else {
+    var redis = require("redis").createClient();
 }
 
 // Original Connection
